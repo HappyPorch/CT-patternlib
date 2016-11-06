e368d26dbfc1c6f1bc061c06996e3584f876813c
Barry O'Kane <barry@endzone.io>
2016-11-06 11:49:32 +1100

    Add missing files to git

---
+++ public/styleguide/data/patternlab-data.js
@@ -0,0 +1,6 @@
+var config = {"cacheBuster":1478393180,"defaultPattern":"all","defaultShowPatternInfo":false,"ishMaximum":"2600","ishMinimum":"240","outputFileSuffixes":{"rendered":"","rawTemplate":"","markupOnly":".markup-only"}};
+var ishControls = {"ishControlsHide":{"hay":"true"}};
+var navItems = {"patternTypes":[{"patternTypeLC":"atoms","patternTypeUC":"Atoms","patternType":"00-Atoms","patternTypeDash":"Atoms","patternTypeItems":[],"patternItems":[{"patternPath":"00-Atoms-00-Colours\/00-Atoms-00-Colours.html","patternSrcPath":"00-Atoms\/00-Colours","patternName":"Colours","patternState":"","patternPartial":"Atoms-Colours"},{"patternPath":"00-Atoms-01-fonts\/00-Atoms-01-fonts.html","patternSrcPath":"00-Atoms\/01-fonts","patternName":"Fonts","patternState":"","patternPartial":"Atoms-fonts"}]}]};
+var patternPaths = {"Atoms":{"Colours":"00-Atoms-00-Colours","fonts":"00-Atoms-01-fonts"}};
+var viewAllPaths = [];
+var plugins = [];
