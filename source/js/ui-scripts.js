8d3ed60514c8ea21e593a7e8c4a336d05a528778
rociofatas <rociofatas@gmail.com>
2016-11-11 20:00:20 +0100

    All behaviour finished
    All behaviour except table

--- source/js/ui-scripts.js
+++ source/js/ui-scripts.js
@@ -1,21 +1,15 @@
-// Footer Menu Mobile Drop Up
-$(window).resize(function(){     
-
-       if ($('.footer-menu-list').css('float') == 'none' ){
-             $('.footer-menu-list').addClass('dropdown-menu');
-       }else{
-       		$('.footer-menu-list').removeClass('dropdown-menu');
-       }
-
-});
-
 // Toggle menu buttons
 $('.header-menu-toggle').click(function(){
     $(this).toggleClass("fa-bars")
            .toggleClass("fa-times");
 });
 
-$('.footer-menu-toggle').click(function(){
-    $(this).toggleClass("fa-minus")
-           .toggleClass("fa-plus");
+$('.footer-menu-btn').click(function(){
+    $('.footer-menu-toggle').toggleClass("fa-minus")
+           					.toggleClass("fa-plus");
 });
+
+$('.info-box-toggle').click(function(){
+	$('.info-box-display').toggleClass('show-info')
+						  .toggleClass('hide-info');	
+})
