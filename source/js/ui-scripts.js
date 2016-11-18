fc6b4f81f9d877c1634aed45773088b889116a4e
Barry O'Kane <barry@endzone.io>
2016-11-18 16:16:27 +1100

    Updated patterns & scripts

--- source/js/ui-scripts.js
+++ source/js/ui-scripts.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+//modal
+$("*[data-modalpop='yes']").on("click", function(e) {
+  e.preventDefault();
+  $('#modalPopup').find('#modalPopup-viewport').attr("src", $(this).attr('href'));
+	$('#modalPopup').find('#modalPopup-viewport').height($(this).attr('data-popinnerheight')?$(this).attr('data-popinnerheight'):"auto");
+  $('#modalPopup').find('#modalPopupTitle').text($(this).attr('data-poptitle'));
+  $('#modalPopup').modal('show');
+  return false;
+});
+
+
 // Toggle menu buttons
 $('.header-menu-toggle').click(function(){
     $(this).toggleClass("fa-bars")
@@ -12,11 +24,17 @@ $('.footer-menu-btn').click(function(){
 $('.info-box-toggle').click(function(){
 	$('.info-box').toggleClass('show-info')
 				  .toggleClass('hide-info');
-	$('.info-box-toggle').toggleClass('active');	
-})
+	$('.info-box-toggle').toggleClass('active');
+});
 
 $('.info-box-toggle').click(function(){
 	$('.info-box-panel').toggleClass('show-info')
 				  .toggleClass('hide-info');
-	$('.info-box-toggle').toggleClass('active');	
-})
+	$('.info-box-toggle').toggleClass('active');
+});
+
+});
+
+function closeModal() {
+	$('#modalPopup').modal('hide');
+}
