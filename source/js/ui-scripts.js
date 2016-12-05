a73efa53934e5ab7a5ded4862f86b7d6af63b3df
Barry O'Kane <barry@endzone.io>
2016-12-05 11:21:59 +1100

    Move "hide" text to inside page info box

--- source/js/ui-scripts.js
+++ source/js/ui-scripts.js
@@ -50,13 +50,11 @@ $('.info-box-toggle').click(function(){
 	$(this).next().toggleClass('show-info')
 				  .toggleClass('hide-info');
 	$(this).toggleClass('active');
-	$(this).next().next().toggle();
 });
 $('.info-box-lower-toggle').click(function(){
-	$(this).toggle();
-	$(this).prev().toggleClass('show-info')
+	$(this).parent().parent().toggleClass('show-info')
 				  .toggleClass('hide-info');
-	$(this).prev().prev().toggleClass('active');
+	$(this).parent().parent().prev().toggleClass('active');
 });
 
 
