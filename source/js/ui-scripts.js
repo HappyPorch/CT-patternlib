0b9933333d8124954d05e4429a86755ae0a3dabe
Barry O'Kane <barry@endzone.io>
2017-10-12 19:31:24 +0100

    Add icon
    Also (shudder) blink function for icons

--- source/js/ui-scripts.js
+++ source/js/ui-scripts.js
@@ -130,3 +130,15 @@ $(function ()
 			event.stopPropagation();
 	})
 });
+
+	$('.blink').each(function() {
+		var elem = $(this);
+		setInterval(function() {
+			if (elem.css('visibility') == 'hidden') {
+				elem.css('visibility', 'visible');
+			} else {
+				elem.css('visibility', 'hidden');
+			}
+		}, 500);
+	});
+	
